ef2b90313db9a5d28708b9382b2b0a038a6dea27
BaoZR <474234165@qq.com>
2024-04-30 17:17:54 +0800

    init

---
+++ src/main/java/com/bob/config/LogHttpRequest.java
@@ -0,0 +1,24 @@
+package com.bob.config;
+
+import java.lang.annotation.*;
+
+/**
+ * @author 犬小哈 （微信号：小哈学Java）
+ * @site www.exception.site
+ * @date 2019/2/12
+ * @time 下午9:19
+ * @discription
+ **/
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+@Documented
+public @interface LogHttpRequest {
+    /**
+     * 日志描述信息
+     *
+     * @return
+     */
+    String description() default "";
+
+}
+
